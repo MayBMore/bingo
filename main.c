d78cbd04a5998f95c4f7b75f321735f02f8ca91f
MayBMore <dh6927@naver.com>
2018-11-14 20:57:19 +0900

    2

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 5 //N을 다음 숫자로 치환한다. 
+#define M //M을 다음 숫자로 치환한다. 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
 	printf("빙고 게임을 시작하지"); 
+	
+	
 	
 	return 0;
 }
